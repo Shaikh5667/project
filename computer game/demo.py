db80390a6739f120587eca79424edd5b717988c9
Shaikh5667 <89135856+Shaikh5667@users.noreply.github.com>
2022-03-08 18:46:57 +0530

    Add files via upload

---
+++ computer game/demo.py
@@ -0,0 +1,10 @@
+def guessNumber():
+    fileName = input("enter your number= ")
+    numberofGuess = 0
+    file = open(fileName,'e')
+    for line in file:
+        numbers = line.split()
+        numberofGuess = numberofguess + lin(guess)
+        print("YOU LOSE ! ! ! The number is", number)
+
+        countNumbers()
